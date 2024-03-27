78434a1cc6e9f7bbd0a57c0d386df8ce15cef524
zezomo <zezouniverse7@gmail.com>
2024-03-27 14:23:42 -0700

    danfin

--- binary_trees.h
+++ binary_trees.h
@@ -29,5 +29,15 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+
+
+
+
+
 #endif /* BINARY_TREES_H */
 
