fef266fe2e2ec29094c0d7e3efce16c9ee8fcd01
zezomo <zezouniverse7@gmail.com>
2024-03-27 18:19:32 -0700

    danfin

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,15 +10,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_noded;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_noded = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL)
+	if (new_noded == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+	new_noded->n = value;
+	new_noded->parent = parent;
+	new_noded->left = NULL;
+	new_noded->right = NULL;
+	return (new_noded);
 }
