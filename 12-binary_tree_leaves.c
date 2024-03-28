9165d5057167a7b8fced4c1280f17ddc8ac4f788
zezomo <zezouniverse7@gmail.com>
2024-03-27 19:36:00 -0700

    danfin

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,7 +9,7 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
     if (tree == NULL)
-        return (0);
+	    return (0);
 
     if (tree->left == NULL && tree->right == NULL)
         return (1);
