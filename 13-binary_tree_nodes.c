820f9b9bc9029f67ae8e9985af4bf7ca6f2f43ba
zezomo <zezouniverse7@gmail.com>
2024-03-27 19:45:17 -0700

    danfin

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ * binary_tree_nodes - Counts the nodes with at least one child intree
  * @tree: Pointer to the root node of the tree to count the nodes
  *
  * Return: Number of nodes with at least one child in the binary tree,
@@ -15,6 +15,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1a);
 }
 
