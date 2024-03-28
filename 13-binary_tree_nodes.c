dd0694eb0dee794478966e30eb52ff223d07e1d3
zezomo <zezouniverse7@gmail.com>
2024-03-27 19:46:15 -0700

    danfin

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,6 +15,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1a);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
 
