f9b51b1587e1515f9537332bb6dfd1a48629b7a1
zezomo <zezouniverse7@gmail.com>
2024-03-27 18:29:27 -0700

    danfin

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,10 +10,12 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
+	binary_tree_t *new_node;
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
+
+        if (parent == NULL)
+		return (NULL);
 	if (new_node == NULL)
 		return (NULL);
 
@@ -21,15 +23,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
 	}
-
 	parent->right = new_node;
-
 	return (new_node);
 }
 
