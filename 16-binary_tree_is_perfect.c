4dbe10fc3b19148615649a422e1efe5ae56eb19c
zezomo <zezouniverse7@gmail.com>
2024-03-29 11:40:32 -0700

    danfin

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,6 +29,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t height = binary_tree_height(tree);
 	size_t nodes = binary_tree_size(tree);
+
 	size_t perfect_nodes = (1 << height) - 1;
 
 	return (nodes == perfect_nodes);
