03f2345ad82ada34c1ffd344ac78128a17808c29
zezomo <zezouniverse7@gmail.com>
2024-03-29 12:31:51 -0700

    danfin

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -27,7 +27,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  *         or NULL if no common ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	       	const binary_tree_t *second)
+		const binary_tree_t *second)
 {
 	size_t depth_first, depth_second;
 
