159a7182d797ec3813256a3f99866c18eabe4c3e
zezomo <zezouniverse7@gmail.com>
2024-03-29 13:12:46 -0700

    danfin

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height.
+ * Return: Height of the tree, or 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid AVL Tree, otherwise 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
+
+	if (abs(left_height - right_height) > 1)
+		return (0);
+
+	if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
+		return (0);
+
+	return (1);
+}
+
