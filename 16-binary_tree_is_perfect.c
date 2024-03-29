d08b00ed4dac41523609c542d9750a12fe696b0d
zezomo <zezouniverse7@gmail.com>
2024-03-29 11:51:19 -0700

    danfin

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,37 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: Pointer to the root node of the tree to check.
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: 1 if the tree is full, 0 otherwise.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if ((tree->left == NULL && tree->right != NULL) ||
+			(tree->left != NULL && tree->right == NULL))
+		return (0);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
 }
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the balance factor.
  *
- * Return: 1 if the tree is perfect, 0 otherwise
+ * Return: Balance factor of the tree, or 0 if tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: Pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = binary_tree_height(tree);
-	size_t nodes = binary_tree_size(tree);
+	if (tree == NULL)
+		return (0);
 
-	size_t perfect_nodes = (1 << height) - 1;
-
-	return (nodes == perfect_nodes);
+	return (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0);
 }
-
